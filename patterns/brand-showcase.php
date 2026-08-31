0fb83b2ad6fee0261ab496003b8decced6a1319c
RochakDhakal <rochakdhakal@gmail.com>
2026-08-31 13:43:38 +0545

    Update: screenshot

--- patterns/brand-showcase.php
+++ patterns/brand-showcase.php
@@ -12,44 +12,37 @@ $consultivo_brand_showcase_img = array(
 	$consultivo_brand_showcase_url . 'assets/images/brand-showcase-3.png',
 	$consultivo_brand_showcase_url . 'assets/images/brand-showcase-4.png',
 	$consultivo_brand_showcase_url . 'assets/images/brand-showcase-5.png',
-	$consultivo_brand_showcase_url . 'assets/images/brand-showcase-6.png',
 );
 ?>
 <!-- wp:group {"metadata":{"patternName":"consultivo/brand-showcase","name":"Brand Showcase","categories":["consultivo-patterns"]},"style":{"spacing":{"padding":{"right":"var:preset|spacing|40","left":"var:preset|spacing|40","top":"0","bottom":"0"},"margin":{"top":"0","bottom":"0"},"blockGap":"0"}},"layout":{"type":"constrained","contentSize":"1260px"}} -->
 <div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:var(--wp--preset--spacing--40);padding-bottom:0;padding-left:var(--wp--preset--spacing--40)"><!-- wp:columns {"verticalAlignment":"center","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|60","left":"var:preset|spacing|80"},"padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80","left":"0","right":"0"},"margin":{"top":"0","bottom":"0"}},"border":{"top":{"width":"0px","style":"none"},"right":{"width":"0px","style":"none"},"bottom":{"color":"var:preset|color|border-color","width":"1px"},"left":{"width":"0px","style":"none"}}}} -->
 <div class="wp-block-columns are-vertically-aligned-center" style="border-top-style:none;border-top-width:0px;border-right-style:none;border-right-width:0px;border-bottom-color:var(--wp--preset--color--border-color);border-bottom-width:1px;border-left-style:none;border-left-width:0px;margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--80);padding-right:0;padding-bottom:var(--wp--preset--spacing--80);padding-left:0"><!-- wp:column {"verticalAlignment":"center"} -->
 <div class="wp-block-column is-vertically-aligned-center"><!-- wp:image {"id":103,"width":"auto","height":"36px","sizeSlug":"full","linkDestination":"none","align":"center"} -->
-<figure class="wp-block-image aligncenter size-full is-resized"><img src="<?php echo esc_url( $consultivo_brand_showcase_img[0] ); ?>" alt="" class="wp-image-103" style="width:auto;height:36px"/></figure>
-<!-- /wp:image --></div>
-<!-- /wp:column -->
-
-<!-- wp:column {"verticalAlignment":"center"} -->
-<div class="wp-block-column is-vertically-aligned-center"><!-- wp:image {"id":113,"width":"auto","height":"32px","sizeSlug":"full","linkDestination":"none","align":"center"} -->
-<figure class="wp-block-image aligncenter size-full is-resized"><img src="<?php echo esc_url( $consultivo_brand_showcase_img[1] ); ?>" alt="" class="wp-image-113" style="width:auto;height:32px"/></figure>
+<figure class="wp-block-image aligncenter size-full is-resized"><img src="<?php echo esc_url($consultivo_brand_showcase_img[0]);?>" alt="" class="wp-image-103" style="width:auto;height:36px"/></figure>
 <!-- /wp:image --></div>
 <!-- /wp:column -->
 
 <!-- wp:column {"verticalAlignment":"center"} -->
 <div class="wp-block-column is-vertically-aligned-center"><!-- wp:image {"id":115,"width":"auto","height":"36px","sizeSlug":"full","linkDestination":"none","align":"center"} -->
-<figure class="wp-block-image aligncenter size-full is-resized"><img src="<?php echo esc_url( $consultivo_brand_showcase_img[2] ); ?>" alt="" class="wp-image-115" style="width:auto;height:36px"/></figure>
+<figure class="wp-block-image aligncenter size-full is-resized"><img src="<?php echo esc_url($consultivo_brand_showcase_img[1]);?>" alt="" class="wp-image-115" style="width:auto;height:36px"/></figure>
 <!-- /wp:image --></div>
 <!-- /wp:column -->
 
 <!-- wp:column {"verticalAlignment":"center"} -->
 <div class="wp-block-column is-vertically-aligned-center"><!-- wp:image {"id":116,"width":"auto","height":"36px","sizeSlug":"full","linkDestination":"none","align":"center"} -->
-<figure class="wp-block-image aligncenter size-full is-resized"><img src="<?php echo esc_url( $consultivo_brand_showcase_img[3] ); ?>" alt="" class="wp-image-116" style="width:auto;height:36px"/></figure>
+<figure class="wp-block-image aligncenter size-full is-resized"><img src="<?php echo esc_url($consultivo_brand_showcase_img[2]);?>" alt="" class="wp-image-116" style="width:auto;height:36px"/></figure>
 <!-- /wp:image --></div>
 <!-- /wp:column -->
 
 <!-- wp:column {"verticalAlignment":"center"} -->
 <div class="wp-block-column is-vertically-aligned-center"><!-- wp:image {"id":117,"width":"auto","height":"36px","sizeSlug":"full","linkDestination":"none","align":"center"} -->
-<figure class="wp-block-image aligncenter size-full is-resized"><img src="<?php echo esc_url( $consultivo_brand_showcase_img[4] ); ?>" alt="" class="wp-image-117" style="width:auto;height:36px"/></figure>
+<figure class="wp-block-image aligncenter size-full is-resized"><img src="<?php echo esc_url($consultivo_brand_showcase_img[3]);?>" alt="" class="wp-image-117" style="width:auto;height:36px"/></figure>
 <!-- /wp:image --></div>
 <!-- /wp:column -->
 
 <!-- wp:column {"verticalAlignment":"center"} -->
 <div class="wp-block-column is-vertically-aligned-center"><!-- wp:image {"id":118,"width":"auto","height":"36px","sizeSlug":"full","linkDestination":"none","align":"center"} -->
-<figure class="wp-block-image aligncenter size-full is-resized"><img src="<?php echo esc_url( $consultivo_brand_showcase_img[5] ); ?>" alt="" class="wp-image-118" style="width:auto;height:36px"/></figure>
+<figure class="wp-block-image aligncenter size-full is-resized"><img src="<?php echo esc_url($consultivo_brand_showcase_img[4]);?>" alt="" class="wp-image-118" style="width:auto;height:36px"/></figure>
 <!-- /wp:image --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns --></div>
